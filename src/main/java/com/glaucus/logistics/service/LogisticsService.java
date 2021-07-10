1c57433ae234bc1d4065147ee99900488822e31e
kartik patel <kartik.patell4july1997@gmail.com>
2021-07-10 12:31:21 +0530

    Second Commit

--- src/main/java/com/glaucus/logistics/service/LogisticsService.java
+++ src/main/java/com/glaucus/logistics/service/LogisticsService.java
@@ -3,6 +3,8 @@ package com.glaucus.logistics.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.glaucus.logistics.model.LogisticModel;
@@ -24,7 +26,7 @@ public class LogisticsService {
 		this.model=model;
 	}
 
-	public  void incrementService() {
+	public ResponseEntity<String> incrementService() {
 		synchronized (lock) {
 			try {
 			int currSeq=repository.findById(0);
@@ -33,6 +35,7 @@ public class LogisticsService {
 			log.info("Incremented index_counter value: {}",currSeq);
 			model.setIndex_counter(currSeq);
 			repository.save(model);
+			return new ResponseEntity<String>("Success",HttpStatus.OK);
 			} catch (DataAccessException e) {
 				log.error("Counter increment operation failed :: error {}", e);
 				throw e;
